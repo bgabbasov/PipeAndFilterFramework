c04b4b0707c6134c84e7aa83e1bccdbd4897e8e6
Bulat Gabbasov <bgabbasov@gmail.com>
2015-11-10 20:40:54 +0300

    First version

---
+++ PipeAndFilterFramework/Filter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PipeAndFilterFramework
+{
+    public abstract class Filter<TIn, TOut>
+    {
+        private readonly Pipe<TIn> _input;
+        private readonly Pipe<TOut> _output;
+        private readonly int _millisecondsTimeout;
+
+        public Filter(Pipe<TIn> input, Pipe<TOut> output, int millisecondsTimeout = 300)
+        {
+            _input = input;
+            _output = output;
+            _millisecondsTimeout = millisecondsTimeout;
+        }
+
+        public void Start(CancellationToken cancellationToken)
+        {
+            Task.Factory.StartNew(() => ProcessInner(cancellationToken), cancellationToken, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+        private void ProcessInner(CancellationToken cancellationToken)
+        {
+            try
+            {
+                while (true)
+                {
+                    if (cancellationToken.IsCancellationRequested) throw new TaskCanceledException();
+                    if (_input.IsCompleted) break;
+
+                    TIn input;
+                    if (_input.TryRead(out input, _millisecondsTimeout, cancellationToken))
+                    {
+                        var output = Process(input);
+                        _output.Write(output);
+                    }
+                }
+            }
+            finally
+            {
+                _output.MarkComplete();
+            }
+        }
+        public abstract TOut Process(TIn input);
+    }
+}
---
+++ PipeAndFilterFramework/Pipe.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PipeAndFilterFramework
+{
+    public class Pipe<T>
+    {
+        private readonly BlockingCollection<T> _queue = new BlockingCollection<T>(new ConcurrentQueue<T>());
+
+        public bool IsCompleted => _queue.IsCompleted;
+
+        public void MarkComplete()
+        {
+            _queue.CompleteAdding();
+        }
+
+        public void Write(T obj)
+        {
+            _queue.TryAdd(obj);
+        }
+        public void Write(T obj, TimeSpan timeout)
+        {
+            _queue.TryAdd(obj, timeout);
+        }
+        public void Write(T obj, int millisecondsTimeout)
+        {
+            _queue.TryAdd(obj, millisecondsTimeout);
+        }
+        public void Write(T obj, int millisecondsTimeout, CancellationToken cancellationToken)
+        {
+            _queue.TryAdd(obj, millisecondsTimeout, cancellationToken);
+        }
+
+        public bool TryRead(out T obj)
+        {
+            return _queue.TryTake(out obj);
+        }
+        public bool TryRead(out T obj, TimeSpan timeout)
+        {
+            return _queue.TryTake(out obj, timeout);
+        }
+        public bool TryRead(out T obj, int millisecondsTimeout)
+        {
+            return _queue.TryTake(out obj, millisecondsTimeout);
+        }
+        public bool TryRead(out T obj, int millisecondsTimeout, CancellationToken cancellationToken)
+        {
+            return _queue.TryTake(out obj, millisecondsTimeout, cancellationToken);
+        }
+    }
+}
