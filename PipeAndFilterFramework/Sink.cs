f69217dd432aa8fd1a09178bbcb9f5b2810f8e6b
Bulat Gabbasov <bgabbasov@gmail.com>
2015-11-10 20:54:13 +0300

    added Source and Sink

---
+++ PipeAndFilterFramework/Sink.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace PipeAndFilterFramework
+{
+    public abstract class Sink<TIn>
+    {
+        private readonly Pipe<TIn> _input;
+        private readonly int _millisecondsTimeout;
+
+        public Sink(Pipe<TIn> input, int millisecondsTimeout = 300)
+        {
+            _input = input;
+            _millisecondsTimeout = millisecondsTimeout;
+        }
+
+        public void Start(CancellationToken cancellationToken)
+        {
+            Task.Factory.StartNew(() => ProcessInner(cancellationToken), cancellationToken, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+        private void ProcessInner(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                if (cancellationToken.IsCancellationRequested) throw new TaskCanceledException();
+                if (_input.IsCompleted) break;
+
+                TIn input;
+                if (_input.TryRead(out input, _millisecondsTimeout, cancellationToken)) Process(input);
+            }
+        }
+        public abstract void Process(TIn input);
+    }
+}
---
+++ PipeAndFilterFramework/Source.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using PipeAndFilterFramework;
+
+namespace PipeAndFilterFramework
+{
+    public abstract class Source<TOut>
+    {
+        private readonly Pipe<TOut> _output;
+        private readonly int _millisecondsTimeout;
+
+        public Source(Pipe<TOut> output, int millisecondsTimeout = 300)
+        {
+            _output = output;
+            _millisecondsTimeout = millisecondsTimeout;
+        }
+
+        protected void Write(TOut obj)
+        {
+            _output.Write(obj);
+        }
+        public void Start(CancellationToken cancellationToken)
+        {
+            Task.Factory.StartNew(() => Process(cancellationToken), cancellationToken, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+        protected abstract void Process(CancellationToken cancellationToken);
+    }
+}
